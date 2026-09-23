0d36e72fed805147d98c29fa2d3b30a3b111a5f3
jenodinerifat59 <mdjenodineislamrifat@gmail.com>
2026-09-24 00:22:07 +0600

    nav bar done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/component/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,10 +21,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
-      lang="en"
+      lang="en" data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Logo from "../../public/assets/logo.png";
+import Link from "next/link";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <Link href="/workouts">Workouts</Link>
+      </li>
+
+      <li>
+        <Link href="/my-plan">My Plan</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-[#0C0D10]">
+      <div className="container mx-auto">
+        <div className="navbar bg-[#0C0D10] shadow-sm">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden text-white"
+              >
+                ☰
+              </div>
+
+              <ul
+                tabIndex={-1}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >{links}</ul>
+            </div>
+            <Link href="/" className="flex items-center gap-2">
+              <Image
+                src={Logo} alt="FITLOG logo" width={35} height={35}/>
+ <p className="text-lg font-black text-white"> FITLOG</p>
+            </Link>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 text-white">
+              {links}
+            </ul>
+          </div>
+          <div className="navbar-end gap-2">
+            <Link
+              href="/my-plan"
+              className="text-white flex items-center gap-2"
+            >Plan <span className="border border-gray-500 rounded-full px-2 py-1 text-[#C2F800]">0</span>
+            </Link>
+            <Link
+              href="/saved"
+              className="text-white flex items-center gap-2"> Saved
+              <span className="border border-gray-500 rounded-full px-2 py-1">0</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
